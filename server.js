dd7b5c948f8cebd9ed77a72c83271e283693b49e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-11-15 16:16:46 +0200

    install knex and update some method apis except signin

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import express, { request, response } from "express";
 import Database from "./data/database.js";
 import cors from "cors";
+import knex from "knex";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "123",
+    database: "smart-brain",
+  },
+});
+
+// db.select("*").from('users')
+//   .then(data => console.log(data));
 
 const app = express();
 
@@ -12,10 +27,12 @@ app.get("/", (request, response) => {
 });
 
 app.post("/signin", (request, response) => {
-  const {email,password } = request.body;
+  const { email, password } = request.body;
 
-  const user = Database.Users.find(user => user.email === email && user.password === password)
-  
+  const user = Database.Users.find(
+    (user) => user.email === email && user.password === password
+  );
+
   if (user) {
     response.json(user);
   } else {
@@ -25,45 +42,61 @@ app.post("/signin", (request, response) => {
 
 app.post("/register", (request, response) => {
   const { email, name, password } = request.body;
-  Database.Users.push({
-    id: "2311",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
 
-  response.json(Database.Users[Database.Users.length - 1]);
+  db("users")
+    .returning("*")
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+    })
+    .then((user) => {
+      response.json(user[0]);
+    })
+    .catch((err) => response.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (request, response) => {
   const { id } = request.params;
-  let found = false;
-  Database.Users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return response.json(user);
-    }
-  });
-
-  if (!found) {
-    response.status(400).json("not found");
-  }
+  db.select("*")
+    .from("users")
+    .where({ id: id })
+    .then((user) => {
+      if (user.length) response.json(user[0]);
+      else response.status(400).json("user not found");
+    });
 });
 
 app.put("/image/:id", (request, response) => {
   const { id } = request.params;
-  const user = Database.Users.find(user => user.id === id);
 
-  if(user){
-    user.entries++;
-    return response.json(user.entries);
-  }else{
-    response.status(400).json("not found");
-  }
+  /**
+   * 
+  knex('books')
+  .where('published_date', '<', 2000)
+  .update({
+    status: 'archived',
+    thisKeyIsSkipped: undefined
+  })
+   */
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      console.log(entries);
+      response.json(entries[0])
+    })
+    .catch(err => response.status(400).json('unable to get entries'))
 
+  // const user = Database.Users.find((user) => user.id === id);
+
+  // if (user) {
+  //   user.entries++;
+  //   return response.json(user.entries);
+  // } else {
+  //   response.status(400).json("not found");
+  // }
 });
-
 
 app.listen(3000);
