19c0e7b21218e8ee3b0c66d5f3374bb019bb0c92
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-11-18 14:22:39 +0200

    update database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt-nodejs";
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-perpendicular-68352",
     port: 5432,
     user: "postgres",
     password: "123",
