7a86382b2457f300c18c20a3352bb71d549d2094
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-11-18 13:32:44 +0200

    add env port variable

--- server.js
+++ server.js
@@ -109,4 +109,6 @@ app.put("/image/:id", (request, response) => {
 
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log(`app is running on port ${process.env.PORT}`)
+});
