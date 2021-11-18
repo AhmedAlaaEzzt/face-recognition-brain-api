89323837e9e2ce11c1797923ca1e0d7e095266b0
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-11-18 14:30:49 +0200

    update database

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt-nodejs";
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl:true
   },
 });
